3cfbb476c74d87a71ed66fc619ef0e4054886508
Augusto Abe <augustotakeo@gmail.com>
2024-08-11 20:05:35 -0300

    feat: RabbitMQ tutorial - Hello World

---
+++ HelloWorld/Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var queueName = "hello";
+
+channel.QueueDeclare(queue: queueName,
+                     autoDelete: false,
+                     exclusive: false,
+                     durable: false,
+                     arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) => 
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Received {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ HelloWorld/Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost"  };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var queueName = "hello";
+
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     autoDelete: false,
+                     exclusive: false,
+                     arguments: null);
+
+var message = "Hello World";
+var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: queueName,
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
