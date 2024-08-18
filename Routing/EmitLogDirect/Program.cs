fc6bbc5a0359c2badc4e1634377b2e21a97a3b1b
Augusto Abe <augustotakeo@gmail.com>
2024-08-18 19:12:56 -0300

    feat: example of sending messages to a group of queues based on rounting key

---
+++ Routing/EmitLogDirect/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var exchange = "direct_logs";
+channel.ExchangeDeclare(exchange, ExchangeType.Direct);
+
+var severity = args.Length > 0 ? args[0] : "info";
+var message = args.Length > 1 ? string.Join(" ", args.Skip(1)) : "Hello World";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange, routingKey: severity, null, body);
+
+Console.WriteLine($"[x] Sent {severity}:{message}");
+Console.ReadLine();
---
+++ Routing/ReceiveLogsDirect/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var exchange = "direct_logs";
+
+channel.ExchangeDeclare(exchange, ExchangeType.Direct);
+
+var queue = channel.QueueDeclare().QueueName;
+
+if(args.Length == 0) {
+    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                            Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+foreach(var severity in args) {
+    channel.QueueBind(queue, exchange, severity);
+}
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var severity = ea.RoutingKey;
+    Console.WriteLine($"Received {severity}:{message}");
+};
+
+channel.BasicConsume(queue, autoAck: true, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
