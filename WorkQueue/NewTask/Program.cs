b05b4633844c8b284bee19addc39af1bf608a9b3
Augusto Abe <augustotakeo@gmail.com>
2024-08-18 15:01:30 -0300

    feat: example of work queue

---
+++ WorkQueue/NewTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// await Task.Delay(60000);
+
+var factory = new ConnectionFactory { HostName = "localhost"  };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var queueName = "hello";
+
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     autoDelete: false,
+                     exclusive: false,
+                     arguments: null);
+
+var message = GetMessage(args);
+var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: queueName,
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args) {
+    return args.Length > 0 ? string.Join(" ", args) : "Hello World";
+}
---
+++ WorkQueue/Worker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// await Task.Delay(60000);
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var queueName = "hello";
+
+channel.QueueDeclare(queue: queueName,
+                     autoDelete: false,
+                     exclusive: false,
+                     durable: false,
+                     arguments: null);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += async (model, ea) => 
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Received {message}");
+
+    var dots = message.Split(".").Length - 1;
+    await Task.Delay(dots*1000);
+
+    Console.WriteLine("[x] Done");
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: false,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
