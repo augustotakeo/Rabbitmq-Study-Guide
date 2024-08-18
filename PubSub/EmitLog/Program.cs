2b07281f3658d4a99684354b4865a51bdceef79a
Augusto Abe <augustotakeo@gmail.com>
2024-08-18 16:12:35 -0300

    feat: simple example of pub/sub pattern

---
+++ PubSub/EmitLog/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var logExchange = "logs";
+channel.ExchangeDeclare(logExchange, ExchangeType.Fanout);
+
+var message = GetMessage();
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(logExchange, routingKey: string.Empty, null, body: body);
+
+string GetMessage() {
+    return args.Length > 0 ? string.Join(" ", args) : "Hello Pub Sub";
+}
---
+++ PubSub/ReceiveLogs/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var logExchange = "logs";
+channel.ExchangeDeclare(logExchange, ExchangeType.Fanout);
+
+var queue = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue, logExchange, routingKey: string.Empty);
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume(queue, autoAck: true, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
